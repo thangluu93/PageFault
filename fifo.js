f8b027988a9d592cc1dcfaefed2ec06bb5e26ce3
thangluu93 <luuthang9300@gmail.com>
2020-05-31 13:41:17 +0700

    done fifo algorithm

---
+++ fifo.js
@@ -0,0 +1,54 @@
+
+
+function inputArray(input) {
+    queue = [];
+
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] != ' ' && queue[i] != ',') {
+            queue.push(parseInt(input[i]));
+        }
+    }
+    return queue;
+
+}
+
+function checkExsit(queue, frameSlot, frame) {
+    //check exist in frame
+    for (let j = 0; j < frameSlot; j++) {
+        if (queue === frame[j]) {
+            return true;
+        }  
+    };
+    return false;
+}
+
+
+ function  pageFault(frameSlot, queue) {
+
+    index = 0; //index of frame
+    let frame = [];
+
+    for (let i = 0; i < queue.length; i++) {
+
+         isExsit =  checkExsit(queue[i], frameSlot, frame);
+        // console.log(isExsit);
+        if (!isExsit) {
+            frame[index] = queue[i];
+            index = ((index + 1) + frameSlot) % frameSlot; //loop from,
+        }
+       console.log(frame);
+    }
+}
+
+
+function main() {
+    let input = '0 1 2 3 0 1 4 0 1 2 3 4';
+    let frameSlot = 3;
+    //Convert string input to array
+    let queue = inputArray(input);
+    pageFault(frameSlot, queue);
+}
+
+
+
+main();
